07fe714af6054cc126496115f18665926bf814d4
Ignat Bojinov <ignatbojinov@gmail.com>
2024-05-17 14:34:35 +0200

    PHP variables

---
+++ PHP/index.php
@@ -0,0 +1,33 @@
+<?php 
+    echo "I like pizza";
+
+    //To declare a varaible we use $
+    $name = "Ignatcho";
+    $food = "pizza";
+
+    $age = 21;
+    $gpa = 2.5;
+
+    $employed = true;
+    $offlie = false;
+
+    echo "Hello {$name}<br>";
+    echo "You like {$food}<br>";
+    echo "You are {$age}<br>";
+    echo "You have {$gpa} gpa<br>";
+    echo "Online status: {$online}<br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
