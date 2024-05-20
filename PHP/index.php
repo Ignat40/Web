8cd64cca93df6afe451f658bc99b6857fbbc564b
Ignat Bojinov <ignatbojinov@gmail.com>
2024-05-20 18:12:05 +0200

    php basics syntax

--- PHP/index.php
+++ PHP/index.php
@@ -17,6 +17,102 @@
     echo "You have {$gpa} gpa<br>";
     echo "Online status: {$online}<br>";
 
+    // Arithmetic Operators
+
+    $x = 10;
+    $y = 4;
+    $z = null;
+
+    $z = $y + $x;
+    $z = $y - $x;
+    $z = $y * $x;
+    $z = $y / $x;
+    $z = $y ** $x;
+    $z = $y % $x;
+
+    echo $z;
+
+    echo "<br>";
+
+    // Increment / Decrement 
+
+    $counter = 0; 
+
+    $counter++;
+    echo "Increase by 1: {$counter}<br>";
+
+    $counter -= 3;
+    echo "Decrease by X: {$counter}<br>";
+
+    //  Operator Precedence 
+    // 1. ()
+    // 2. **
+    // 3. * / %
+    // 4. + / - 
+
+    echo "<br>";
+
+    // Arrays -> holds more than one value at a time 
+
+    $food1 = "apple";
+    $food2 = "banana";
+    $food3 = "pineapple";
+    $food4 = "lemon";
+
+    $foodsArr = array("bread","pancakes","peanutbutter","idk");
+
+    // Print a single Item
+    echo $foodsArr[0] . "<br>";
+    echo $foodsArr[1] . "<br>";
+    echo $foodsArr[2] . "<br>";
+    echo $foodsArr[3] . "<br>";
+
+    //Change Items
+    $foodsArr[2] = $food2;
+
+    // Array functions 
+    array_push($foodsArr, "pineapple");//adds el at the back of the arr
+    array_pop($foodsArr);//removes last el in arr
+    array_shift($foodsArr);//removes the first el and shifts the other
+    $reversed_array = array_reverse($foodsArr);
+    echo count($foodsArr) ."<br>";
+
+    foreach($foodsArr as $food){
+        echo $food ."<br>";
+    }
+
+    echo "<br>";
+
+    // Associative array -> array made of key/value pairs
+    // Basically a dictionary dude 
+
+    // countires -> capitals
+    // id -> username 
+    // item -> price 
+
+    $capitals = array("USA" => "Washington D.C." , 
+                      "Japan" => "Tokyo", 
+                      "India" => "New Delhi", 
+                      "Bulgaria" => "Sofia");
+
+    echo $capitals['Bulgaria'] . "<br>"; // prints the capital
+
+    //Functions connected with Associtative array
+    $capitals['USA'] = 'Las Vegas';
+    $capitals['Denmark'] = 'Copenhagen'; // adds at the end of the array
+    array_pop(($capitals));
+    array_shift($capitals);
+    $keys = array_keys($capitals);
+    $values = array_values($capitals);
+    $capitals = array_flip($capitals);
+    $capitals = array_reverse($capitals);
+
+    foreach($capitals as $key => $value ){
+        echo "{$key} = {$value} <br>";
+    }
+
+    
+
 ?>
 
 <!DOCTYPE html>
