31adba6cb7aa334bc6a216ad24af3a74dbbcaf55
Ignat Bojinov <ignatbojinov@gmail.com>
2024-05-21 21:54:26 +0200

    include function files

---
+++ PHP/Include()/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    // include() = Copies the contents of a file (php/html/text)
+    //             and includes it in you php file.
+    //             Sections of out website become reusable
+    //             Changes only need to be moade in one place. 
+
+    include("header.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is my homepade!<br>
+    Home page stuff do here!
+</body>
+</html>
+
+<?php 
+    include("footer.html");
+?>
